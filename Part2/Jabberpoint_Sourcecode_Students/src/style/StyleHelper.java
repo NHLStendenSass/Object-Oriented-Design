d9f94037654bc00d07a61b934616dd446fbd5e6d
Sander Siimann <58694306+SanderSass@users.noreply.github.com>
2024-04-21 17:28:08 +0200

    update

--- Part2/Jabberpoint_Sourcecode_Students/src/style/StyleHelper.java
+++ Part2/Jabberpoint_Sourcecode_Students/src/style/StyleHelper.java
@@ -15,11 +15,11 @@ public class StyleHelper {
      */
     public static void createStyles() {
         styles = new Style[5];
-        styles[0] = new Style(0, Color.red, 48, 20);
-        styles[1] = new Style(20, Color.blue, 40, 10);
-        styles[2] = new Style(50, Color.black, 36, 10);
-        styles[3] = new Style(70, Color.black, 30, 10);
-        styles[4] = new Style(90, Color.black, 24, 10);
+        styles[0] = new Style(0, Color.red, 48, 20);    // style voor item-level 0
+        styles[1] = new Style(20, Color.blue, 40, 10);  // style voor item-level 1
+        styles[2] = new Style(50, Color.black, 36, 10); // style voor item-level 2
+        styles[3] = new Style(70, Color.black, 30, 10); // style voor item-level 3
+        styles[4] = new Style(90, Color.black, 24, 10); // style voor item-level 4
     }
 
     /**
